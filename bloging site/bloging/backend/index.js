12075583d309b6a8b6ab324553d0dbef8c575bd5
utsahice <utsahic.encircle@gmail.com>
2025-10-17 17:10:55 +0530

    Advanced Node.js Concepts in progress comment done design left

---
+++ bloging site/bloging/backend/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:5173', 'http://localhost:3001'], 
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3001'],
+  credentials: true,
+}));
+app.use(express.json());
+
+const comments = [];
+
+// API to post comment
+app.post('/comments', (req, res) => {
+  const { author, content } = req.body;
+  if (!author || !content) {
+    return res.status(400).json({ message: 'Author and content required' });
+  }
+
+  const comment = { id: Date.now(), author, content };
+  comments.push(comment);
+
+  io.emit('newComment', comment); 
+
+  res.status(201).json(comment);
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.emit('allComments', comments);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000');
+});
